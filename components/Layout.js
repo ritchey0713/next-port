f4cc0c97c6c8bceeea7fbed0dd1dc240eeb0ac8e
Dj <ritchey0713@gmail.com>
2020-11-24 14:00:21 -0500

    add style tags

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,61 @@
 import Link from "next/link";
 
 export default ({ children, title }) => (
-  <div>
+  <div className="root">
     <header>
-      <Link href="/">Home</Link>
-      <Link href="/about">About</Link>
-      <Link href="/hireme">Hire me</Link>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+      <Link href="/hireme">
+        <a>Hire me</a>
+      </Link>
     </header>
     <h1>{title}</h1>
     {children}
 
     <footer>footer</footer>
+    <style jsx>{`
+      .root {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+
+      header {
+        width: 100%;
+        display: flex;
+        justify-content: space-around;
+        padding: 1em;
+        font-size: 1.2rem;
+        background: indigo;
+      }
+
+      header a {
+        color: darkgrey;
+        text-decoration: none;
+      }
+      header a:hover {
+        font-weight: bold;
+        color: lightgrey;
+      }
+
+      footer {
+        padding: 1em;
+      }
+    `}</style>
+
+    <style global jsx>
+      {`
+        body {
+          margin: 0;
+          font-size: 110%;
+          background: #f0f0f0;
+        }
+      `}
+    </style>
   </div>
 );
