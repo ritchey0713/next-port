c054976ffd70ae157edee0ff092a83d4ce830a44
Dj <ritchey0713@gmail.com>
2020-11-24 13:26:49 -0500

    add hireme page

---
+++ pages/hireme.js
@@ -0,0 +1,10 @@
+const HireMe = () => (
+  <div>
+    <h1>Hire me</h1>
+    <p>
+      You cna hire me at <a href="mailto:google.com"> google.com</a>
+    </p>
+  </div>
+);
+
+export default HireMe;
