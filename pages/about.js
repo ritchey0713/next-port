959dd472fb3339e9835dd151727630669ffea502
Dj <ritchey0713@gmail.com>
2020-11-25 10:21:52 -0500

    add getInitProps

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,29 @@
 import Link from "next/link";
+import { Component } from "react";
 import Layout from "../components/Layout";
 
-const About = () => (
-  <Layout title="About">
-    <p>A JS programmer</p>
-    <img src="/static/js-logo.png" alt="logo" />
-  </Layout>
-);
+class About extends Component {
+  static async getInitialProps() {
+    // fetch("https://api.github.com/users/ritchey0713")
+    //   .then((resp) => resp.json())
+    //   .then((data) => {
+    //     console.log(data);
+    //   });
+    // return { user: data };
+    const resp = await fetch("https://api.github.com/users/ritchey0713");
+    const data = await resp.json();
+    return { user: data };
+  }
 
+  render() {
+    const { user } = this.props;
+    return (
+      <Layout title="About">
+        <p>{user.name}</p>
+        <p>{user.bio}</p>
+        <img src={user.avatar_url} alt="me" />
+      </Layout>
+    );
+  }
+}
 export default About;
