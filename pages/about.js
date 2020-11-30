c3390794392bd53b59c60be1e1114eed921256d2
Dj <ritchey0713@gmail.com>
2020-11-30 10:31:57 -0500

    add error page to bad loads

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Component } from "react";
 import Layout from "../components/Layout";
+import Error from "../pages/_error";
 
 class About extends Component {
   static async getInitialProps() {
@@ -10,13 +11,19 @@ class About extends Component {
     //     console.log(data);
     //   });
     // return { user: data };
-    const resp = await fetch("https://api.github.com/users/ritchey0713");
+    const resp = await fetch("https://api.github.com/users/ritchey0713111");
+    const statusCode = resp.status > 200 ? resp.status : false;
     const data = await resp.json();
-    return { user: data };
+    return { user: data, statusCode };
   }
 
   render() {
-    const { user } = this.props;
+    const { user, statusCode } = this.props;
+
+    if (statusCode) {
+      return <Error statusCode={statusCode} />;
+    }
+
     return (
       <Layout title="About">
         <p>{user.name}</p>
